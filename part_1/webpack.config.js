121690d12a3c09a2dd50944a9175377202d49440
Mariana Svirzhevska <mariana_svirzhevska@epam.com>
2022-06-02 23:09:04 +0300

    HW-2 Link and Script tag attributes

--- part_1/webpack.config.js
+++ part_1/webpack.config.js
@@ -19,6 +19,18 @@ module.exports = {
                     outputPath: 'images',
                 },
             },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                  loader: 'babel-loader',
+                  options: {
+                    presets: [
+                      ['@babel/preset-env']
+                    ]
+                  }
+                }
+              }
         ]
     },
     plugins: [
